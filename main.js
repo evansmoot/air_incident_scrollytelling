06d7d1a61112c52d909b6a0e5fc92452857fa158
evansmoot <smootev@msn.com>
2019-11-24 13:07:48 -0500

    Added some basic scrolling and scroller.js file

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ function start() {
 
         //Filter out any data without longitude and latitude since we can't map those
         var incidents = data.filter(function (d) {
-            return (d.Longitude != "" && d.Latitude != "");
+            return (d.Longitude != "" || d.Latitude != "");
         });
+
+        //Sort incidents by date since we want our scrolling to be time-based
+        incidents.sort(function(a,b) {
+            return new Date(a.Event_Date) - new Date(b.Event_Date);
+        });
+        //Probably should break the data into ~10 different arrays based on years
+        //so they can appear/disappear easily as we scroll
+
         console.log(incidents);
     });
 }
