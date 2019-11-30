eb9fe7b43b2224617e7b8c0afe5fe3edc503ac65
jhwang81@gatech.edu <jhwang81@gatech.edu>
2019-11-30 01:51:13 -0600

    Removed filter and sorting for data since unneeded

--- main.js
+++ main.js
@@ -44,14 +44,12 @@ function start() {
   d3.csv('data.csv', function(error, data) {
 
       //Filter out any data without longitude and latitude since we can't map those
-      incidents = data.filter(function (d) {
-          return (d.Longitude != "" || d.Latitude != "") && d.Country == "United States";
-      })
+      incidents = data;
 
       //Sort incidents by date since we want our scrolling to be time-based
-      incidents.sort(function(a,b) {
+      /*incidents.sort(function(a,b) {
           return new Date(a.Event_Date) - new Date(b.Event_Date);
-      });
+      });*/
       //Probably should break the data into ~10 different arrays based on years
       //so they can appear/disappear easily as we scroll
 
@@ -60,43 +58,43 @@ function start() {
       while(incidents.length) {
           switch(index) {
               case 1:
-                firstArray = incidents.splice(0,56);
+                firstArray = incidents.splice(0,200);
                 console.log(firstArray);
                 break;
               case 2:
-                secondArray = incidents.splice(0,56);
+                secondArray = incidents.splice(0,200);
                 console.log(secondArray);
                 break;
               case 3:
-                thirdArray = incidents.splice(0,56);
+                thirdArray = incidents.splice(0,200);
                 console.log(thirdArray);
                 break;
               case 4:
-                fourthArray = incidents.splice(0,56);
+                fourthArray = incidents.splice(0,200);
                 console.log(fourthArray);
                 break;
               case 5:
-                fifthArray = incidents.splice(0,56);
+                fifthArray = incidents.splice(0,200);
                 console.log(fifthArray);
                 break;
               case 6:
-                sixthArray = incidents.splice(0,56);
+                sixthArray = incidents.splice(0,200);
                 console.log(sixthArray);
                 break;
               case 7:
-                seventhArray = incidents.splice(0,56);
+                seventhArray = incidents.splice(0,200);
                 console.log(seventhArray);
                 break;
               case 8:
-                eighthArray = incidents.splice(0,56);
+                eighthArray = incidents.splice(0,200);
                 console.log(eighthArray);
                 break;
               case 9:
-                ninthArray = incidents.splice(0,56);
+                ninthArray = incidents.splice(0,200);
                 console.log(ninthArray);
                 break;
               case 10:
-                tenthArray = incidents.splice(0,56);
+                tenthArray = incidents.splice(0,200);
                 console.log(tenthArray);
                 break;
             }
